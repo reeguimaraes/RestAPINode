37b5aaab3b0d0c924f520fc8425375be71dde00e
Renato Guimarães <r_guiimaraes@hotmail.com>
2022-10-29 16:26:14 -0300

    express

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const http = require('http');
-const port = 3000;
+import app from './src/app.js'
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Curso de Node');
-})
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+
+
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
-});
+})
