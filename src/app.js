851f4bf93c2193af99f95e8c244bebc38c627bea
Renato Guimarães <r_guiimaraes@hotmail.com>
2022-10-29 19:29:31 -0300

    controller completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/Livro.js";
-import livro from "./models/Livro.js";
 import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'erro de conexão'))
@@ -10,36 +8,8 @@ db.once("open", () => {
 })
 
 const app = express();
-
 app.use(express.json());
-
 routes(app)
 
-//const livros = [
-//    {id: 1, "titulo": "Senhor dos Aneis"},
-//    {id: 2, "titulo": "O Hobbit"}
-// ]
-
-app.get('/livros/:id',(req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
-
-app.put('/livros/:id',(req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros)
-})
-
-app.delete('/livros/:id',(req, res) => {
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso}`);
-})
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id)
-}
 
 export default app
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,7 +5,11 @@ const router = express.Router();
 
 router 
     .get("/livros", LivroController.listarLivros)
+    .get("/livros/:id", LivroController.listarLivroPorId)
     .post("/livros", LivroController.cadastrarLivro)
+    .put("/livros/:id", LivroController.atualizarLivro)
+    .delete("/livros/:id", LivroController.excluirLivro); 
+    
 
 export default router;
 
